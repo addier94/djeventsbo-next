7c977aed789afc1b0c43a062c2920f931784960b
Alfredo Fernandez Carlos <alfredofernandezcarlos@gmail.com>
2021-07-27 12:07:13 -0400

    Switch to getServerSideProps in slug page

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
-import Link from 'next/link'
+import Link from "next/link";
 import Layout from "@/components/Layout";
-import EventItem from '@/components/EventItem'
-import {API_URL} from '@/config/index';
+import EventItem from "@/components/EventItem";
+import { API_URL } from "@/config/index";
 
-export default function HomePage({events}) {
+export default function HomePage({ events }) {
   return (
     <Layout>
       <h1>Próximos eventos</h1>
@@ -14,20 +14,20 @@ export default function HomePage({events}) {
       ))}
 
       {events.length > 0 && (
-        <Link href='/events'>
-          <a className='btn-secondary'>Ver todos los eventos</a>
+        <Link href="/events">
+          <a className="btn-secondary">Ver todos los eventos</a>
         </Link>
       )}
     </Layout>
-  )
+  );
 }
 
 export async function getStaticProps() {
-  const res = await fetch(`${API_URL}/events?_sort=date:ASC&_limit=3`)
-  const events = await res.json()
+  const res = await fetch(`${API_URL}/events?_sort=date:ASC&_limit=3`);
+  const events = await res.json();
 
   return {
     props: { events },
-    revalidate: 1
-  }
+    revalidate: 1,
+  };
 }
